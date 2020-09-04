d28a5fa6b76909714d97eac6d914556b855b5a31
mayorau3 <mayo1129rau1206@gmail.com>
2020-09-04 18:52:28 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -29,12 +29,18 @@ class CalendarsController < ApplicationController
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
-    7.times do |x|
+    7.times do |x| # Date.today.wdayの戻り値である曜日の数字をこの中で1ずつ増やして行く
       today_plans = []
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      
+      wday_num =  Date.today.wday + x #Date.today.wdayを利用して添字となる数値を得る
+      #もしもwday_numが7以上であれば、7を引く
+      if wday_num >= 7 #条件式を記述
+        wday_num = wday_num - 7
+      end
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :wday => wdays[wday_num], :plans => today_plans} #←曜日を追加する
       @week_days.push(days)
     end
 
